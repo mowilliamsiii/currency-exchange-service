9f133e33f024b5583a04b4dfa46b2282820fea9d
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-08-19 10:56:11 -0400

    CircuitBreakerController configurations

---
+++ src/main/java/com/moseswilliamsiii/currencyexchangeservice/controllers/CircuitBreakerController.java
@@ -0,0 +1,31 @@
+package com.moseswilliamsiii.currencyexchangeservice.controllers;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    public String sampleApi(){
+
+        logger.info("called received");
+
+        ResponseEntity<String> forEntity = new RestTemplate()
+                .getForEntity("http://localhost:8080/dummpy-api", String.class);
+
+        return forEntity.getBody();
+    }
+
+    public String hardCodedResponse(Exception ex){
+        return "fallback-response";
+    }
+}
